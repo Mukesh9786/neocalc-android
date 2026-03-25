8bbe6cb7f4741b099cce48209d7cc8f796bf79bd
Mukesh D <mukeshdanuskodi@gmail.com>
2026-03-25 16:03:24 +0530

    add: service worker for PWA

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'neocalc-v1';
+const ASSETS = [
+  '/neocalc-android/',
+  '/neocalc-android/index.html',
+  '/neocalc-android/styles.css',
+  '/neocalc-android/script.js',
+  '/neocalc-android/icon.png',
+  '/neocalc-android/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
